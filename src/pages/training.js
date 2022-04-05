573c1738b0f8e661ea197694a8f3dd10d8846310
Kha Le <minhkhanb@gmail.com>
2022-04-05 20:07:19 +0700

    init

---
+++ src/pages/training.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TrainingHome from '../sections/TrainingHome';
+
+const TrainingPage = () => {
+  return (
+    <div>
+      <h1>Training</h1>
+      <TrainingHome />
+    </div>
+  );
+};
+
+export default TrainingPage;
---
+++ src/sections/TrainingHome/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TrainingHome = () => {
+  return (
+    <div>
+      <h1>Training Home</h1>
+    </div>
+  );
+};
+
+export default TrainingHome;
